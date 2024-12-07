8a5db403fc145199f28e75097f37c850489336ee
Luk <lucapierini03@gmail.com>
2024-12-07 16:59:25 -0300

    Creación de archivos de base de datos, controller y routes

--- routes/authRouter.go
+++ routes/authRouter.go
@@ -1,1 +1,15 @@
 package routes
+
+import (
+	
+	"github.com/gin-gonic/gin"
+	controller "github.com/lucapierini/UserAutentication/controllers"
+)
+
+func AuthRouters(incomingRoutes *gin.Engine) {
+	auth := incomingRoutes.Group("/users")
+	{
+		auth.POST("/singup", controller.Singup())
+		auth.POST("/login", controller.Login())
+	}
+}

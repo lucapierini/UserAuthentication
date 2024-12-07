ae9e065163ef2d3d0c8db6286ad2e406008246da
Luk <lucapierini03@gmail.com>
2024-12-07 17:10:00 -0300

    Avances de authRouter y userRouter

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,2 +1,13 @@
 package routes
 
+import (
+	controller "github.com/lucapierini/UserAutentication/controllers"
+	"github.com/gin-gonic/gin"
+	"github.com/lucapierini/UserAutentication/middleware"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/users/:id", controller.GetUser())
+}

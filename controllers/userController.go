7478b8b6d42903da0d0de73e5cec29fa61667ba9
Luca-Pierini-GitH <lucapierini03@gmail.com>
2024-12-09 18:35:23 -0300

    Add function VerifyPassword, add functions to tokenHelper

--- controllers/userController.go
+++ controllers/userController.go
@@ -12,6 +12,11 @@ import(
 	"github.com/lucapierini/UserAuthentication/models"
 	helpers "github.com/lucapierini/UserAuthentication/helpers"
 	"golang.org/x/crypto/bcrypt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/primitive"
+	"github.com/lucapierini/UserAuthentication/database"
+)
 	
 	var userCollection *mongo.Collection = database.OpenCollection(database.Client, "users")
 
@@ -21,14 +26,21 @@ import(
 		
 	}
 
-	func VerifyPassword(hashedPassword, password string) error {
-		
+	func VerifyPassword(userPassword string, providedPassword string)(bool, string) {
+		err := bcrypt.CompareHashAndPassword([]byte(providedPassword), []byte(userPassword))
+		check := true
+		msg := " "
+
+		if err != nil {
+			msg = "email or password are incorrect"
+			check = false
+		}
 	}
 
 	func Signup()gin.HandlerFunc{
 		return func(c *gin.Context) {
 			var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-			var user models. User
+			var user models.User
 			
 			if err := c.BindJSON(&user); err != nil {
 				c.JSON(http. StatusBadRequest, gin.H{"error": err.Error()})
@@ -48,7 +60,7 @@ import(
 				c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while checking user email"})
 			}
 
-			count, err := userCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+			count, err = userCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
 			defer cancel()
 			if err != nil{
 				log.Panic(err)
@@ -59,10 +71,46 @@ import(
 				c.JSON(http.StatusBadRequest, gin.H{"error": "email or phone already in use"})
 				return
 			}
+			
+			user.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+			user.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+			user.ID = primitive.NewObjectID()
+			user.User_id = user.ID.Hex()
+			token, refreshToken, _ := helpers.GenerateAllTokens(user.Email, *user.First_name, *user.Last_name, *user.User_type, *user.User_id)
+			user.Token = &token
+			user.RefreshToken = &refreshToken
+			
+			resultInsertionNumber, insertErr := userCollection.InsertOne(ctx, user)
+			if insertErr != nil {
+				msg := fmt.Sprintf("User item was not created")
+				c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+				return
 		}
+		defer cancel()
+		c.JSON(http.StatusOK, gin.H{"data": resultInsertionNumber})
+	}
+}
 
-	func Login(c *gin.Context) {
-		
+	func Login() gin.HandlerFunc {
+		return func(c *gin.Context){
+			var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+			var user models.User
+			var foundUser models.User
+
+			if err := c.BindJSON(&user); err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+				return
+			}
+			err := userCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&foundUser)
+			defer cancel()
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "invalid credentials"})
+				return
+			}
+
+			passwordIsValid, msg := VerifyPassword(*user.Password, *foundUser.Password)
+			defer cancel()
+		}
 	}
 
 	func GetUser() gin.HandlerFunc {
@@ -92,4 +140,3 @@ import(
 	func GetUsers(c *gin.Context) {
 		
 	}
-)
--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -1,1 +1,64 @@
 package helpers
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+	"github.com/lucapierini/UserAuthentication/database"
+	jwt "github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitives"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type SignedDetails struct {
+	Email string
+	First_name string
+	Last_name string
+	Uid string
+	User_type string
+	jwt.StandardClaims
+}
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "users")
+
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
+
+func GenerateAllTokens(email string, firstName string, lastName string, uid string, userType string) (signedToken string, signedRefreshToken string, err error) {
+	claims := &SignedDetails{
+		Email: email,
+		First_name: firstName,
+		Last_name: lastName,
+		Uid: uid,
+		User_type: userType,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
+
+	refreshClaims := &SignedDetails{
+		// Email: email,
+		// First_name: firstName,
+		// Last_name: lastName,
+		// Uid: uid,
+		// User_type: userType,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(168)).Unix(),
+		},
+	}
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		return
+	}
+
+	refreshToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		return
+	}
+
+	return token, refreshToken, err
+}

0f839944ed5b8630e1521a524f2143bb20ffa433
Luca-Pierini-GitH <lucapierini03@gmail.com>
2024-12-11 19:29:35 -0300

    Avances en tokenHelper y auth Middleware

--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -65,6 +65,33 @@ func GenerateAllTokens(email string, firstName string, lastName string, uid stri
 	return token, refreshToken, err
 }
 
+func ValidateToken(signedToken string) (claims *SignedDetails, msg string) {
+	token, err := jwt.ParseWithClaims(signedToken, &SignedDetails{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(SECRET_KEY), nil
+	},
+)
+
+if err != nil {
+	msg = err.Error()
+	return
+}
+
+claims, ok := token.Claims.(*SignedDetails)
+if !ok {
+	msg = fmt.Sprint("the token is invalid")
+	msg = err.Error()
+	return
+}
+
+if claims.ExpiresAt < time.Now().Local().Unix() {
+	msg = fmt.Sprint("token is expired")
+	msg = err.Error()
+	return
+}
+return claims, msg
+	
+}
+
 func UpdateAllTokens(singedToken string, signedRefreshToken string, userId string) {
 	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
